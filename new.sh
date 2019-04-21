c260bbd8b7ff5d4c40f1f9a1f7b7da6de6d40e16
IqbalNMm <49842073+IqbalNMm@users.noreply.github.com>
2019-04-22 00:20:34 +0700

    Add files via upload

--- new.sh
+++ new.sh
@@ -1,130 +1,66 @@
+#usr/bin/bash
 clear
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
 pur='\033[35;1m' #purple
 cy='\033[36;1m' #cyan
 me='\033[31;1m' #merah
-pu='\033[37;1m' #purple
+pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
-echo
-echo
-python2 meizu.py
-echo
-clear
-echo
-echo $red"          Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa sayang :v("   
-sleep 1
-echo '''\a
-\033[34;1m                              _
-\033[34;1m                             / |
-\033[34;1m                             | |
-\033[34;1m                             | |
-\033[34;1m                            _|_|_ '''
-sleep 1
-apt update && apt upgrade
-apt install nano
-apt install git
-pkg install python python2 vim figlet curl
-clear
+# Jangan Recode Ya gayn
 echo
 echo
 echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     INDONESIA DARK TERMUX ASSOCIATE"$i"         |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku"MR.Link :👇
+https://www.facebook.com/Z.hani10  [FR13ND8]"$i"   |"
+echo $i" |"$pu" FACEBOOK :"$ku" Z HANI"$i"               |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"  |"
+echo $i" |"$cy"            ~ [cyber] [%MR.S%] [BADRU]"$i" |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 0895375872878"$i"            |"
+echo $i" |"$pu" Contack Gmail :"$ku" Muhamadbadruwasih8@gmail.com"$i"|"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa sayang :v"
-sleep 1
-echo '''\a
-\033[32;1m                             ____
-\033[32;1m                            |___ \
-\033[32;1m                             __) |
-\033[32;1m                            / __/
-\033[32;1m                           |_____| '''
-sleep 1
-apt install php
-pip2 install mechanize
-pip2 install lolcat
-pip2 install requests
-clear
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" SPAM CALL Tanpa Batas    "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" BOT REACTION FB          "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" PROFILE GUARD            "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" SPAM WHATSAPPS           "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" OSIF                     "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" F"$i" |"
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me"10"$i" |"$cy" LACAK LOKASI             "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
+echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"────────────────────────────────    ─────"
 echo
-echo
-echo
-echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa sayang :v"
-sleep 1
-echo '''\a
-\033[35;1m                            _____
-\033[35;1m                           |___ /
-\033[35;1m                             |_ \
-\033[35;1m                            ___) |
-\033[35;1m                           |____/ '''
-sleep 1
-clear
-echo
-echo
-echo
-echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa sayang :v"
-sleep 1
-echo '''\a
-\033[33;1m                            _  _
-\033[33;1m                           | || |
-\033[33;1m                           | || |_
-\033[33;1m                           |__   _|
-\033[33;1m                              |_| '''
-sleep 1
-clear
-echo
-echo
-echo
-echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa sayang :v"
-sleep 1
-echo '''\a
-\033[36;1m                             ____
-\033[36;1m                            | ___|
-\033[36;1m                            |___ \
-\033[36;1m                             ___) |
-\033[36;1m                            |____/ '''
-sleep 1
-clear
-echo
-echo $pur"==================="$cy" ==================="
-echo $i"Ôûê"$pu"  AUTHOR  :  Z HAHANI                "$i"  Ôûê"
-echo $i"Ôûê"$pu"  WhatsApp:  0895375872878"$i"              Ôûê"
-echo $i"Ôûê"$pu"  GMAIL   :  muhamadbadruwasih8@gmail.com"$i" Ôûê"
-echo $pur"==================="$cy" ==================="
-echo
-echo $cy"MeNu BRUTEPORCE NeWS:"
-echo $ku"["$me"1"$ku"]"$i" REPORT FB"
-echo $ku"["$me"2"$ku"]"$i" BRUTE PORCE"
-echo $ku"["$me"3"$ku"]"$i" BRUTE FORCE FB CRACKER"
-echo $ku"["$me"4"$ku"]"$i" MBF"
-echo $ku"["$me"5"$ku"]"$i" CLONING YAHOO"
-echo $ku"["$me"6"$ku"]"$i" OSIF"
-echo $ku"["$me"7"$ku"]"$i" PROFILEGUARD FB"
-echo $ku"["$me"8"$ku"]"$i" BOT REACTION FACEBOOK"
-echo $ku"["$me"9"$ku"]"$i" SPAM CALL"
-echo $ku"["$me"10"$ku"]"$i" SPAM WHATSHAPP"
-echo $ku"["$me"11"$ku"]"$i" YOUTUBE-DL"
-echo $ku"["$me"12"$ku"]"$i" LACAK LOKASI"
-echo $i"["$me"13"$i"]"$ku" EXIT"
-echo
-echo $pur"Ôò¡ÔöÇ["$pur"Masukkan pilihan anda"$pur"]"
-read -p"Ôò¦ÔöÇ>> " pil
+echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
 
 if [ $pil = 1 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/IlayTamvan/Report.git
-cd Report
-unzip Report.zip
-python2 Report.py
+git clone https://github.com/Muhamadba/MBFz 
+cd MBFz 
+ ls
+php SpamCall.php
 fi
 
 if [ $pil = 2 ]
@@ -132,9 +68,9 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/Senitopeng/fbbrute.git
-cd fbbrute
-python2 jomblo.py
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
 fi
 
 if [ $pil = 3 ]
@@ -142,9 +78,11 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
-cd Fb-Cracker-v.3
-python2 crack.py
+pkg install python2
+pkg install git
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
 fi
 
 if [ $pil = 4 ]
@@ -152,8 +90,8 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
-cd multi-bruteforce-facebook
+git clone https://github.com/FR13ND8/mbf
+cd mbf
 python2 MBF.py
 fi
 
@@ -162,11 +100,9 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/wahyuandhika/YahooCloning.git
-cd YahooCloning
-pip2 install requests mechanize
-pip2 install requirements
-python2 cloning.py
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
 fi
 
 if [ $pil = 6 ]
@@ -185,9 +121,12 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/jaxBCD/FBshield.git
-cd FBshield
-python2 guard.py
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+
 fi
 
 if [ $pil = 8 ]
@@ -195,9 +134,9 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/edi-nasa12/ROBOT.git
-cd ROBOT
-python2 blackbot.py
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
 fi
 
 if [ $pil = 9 ]
@@ -205,32 +144,13 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/Aditya021/SpamCall
-cd SpamCall
-php SpamCall.php
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
 fi
 
 if [ $pil = 10 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/4L13199/LITESPAM.git
-cd LITESPAM
-sh LITESPAM.sh
-fi
-
-if [ $pil = 11 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/rahmadxyz/sub-bot.git
-cd sub-bot
-php sub-bot.php
-fi
-
-if [ $pil = 12 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
@@ -240,22 +160,112 @@ cd locator
 bash locator.sh
 fi
 
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
 if [ $pil = 13 ]
 then
 clear
-figlet -f slant "W A I T"|Ulolcat
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
 sleep 2
-echo $cy"Terima Kasih sudah mengunakan Tools ini"
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
 sleep 2
-echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
 sleep 2
-echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
-sleep 2
-echo $ku"Facebook :"$i" Z HANI"
 echo $ku"WhatsApp :"$i" 0895375872878"
+echo $ku"Facebook :"$i" MUHAMAD BADRU"
 sleep 2
-echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
-sleep 2
-echo $pur"Z hani { "
+echo $pur">> Thanks Yang Sudah Support Saya <<"
 exit
 fi
